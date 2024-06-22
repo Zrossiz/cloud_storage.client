d7c1f635377ee32cd260b45ea5d0fd2b35bf5c5d
Zrossiz <soon.pluxury@mail.ru>
2024-06-22 08:59:14 +0300

    chore: added background for sign up

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,6 +1,16 @@
+import styles from '../styles/SignUp.module.scss';
+import Image from 'next/image';
+
 const SignUpPage = () => {
     return (
-        <div>sign up</div>
+        <div className={styles.pageWrapper}>
+            <div className={styles.background}>
+                <Image src='/Background.jpg' fill alt="Background" />
+            </div>
+            <div className={styles.formWrapper}>
+                Форма
+            </div>
+        </div>
     )
 };
 
