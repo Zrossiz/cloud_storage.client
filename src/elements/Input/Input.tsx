2f153e3fee9f0f40219be37e30af511df3cd64a3
Zrossiz <soon.pluxury@mail.ru>
2024-06-22 09:08:33 +0300

    feat: added input element boilerplate

---
+++ src/elements/Input/Input.tsx
@@ -0,0 +1,8 @@
+import { InputProps } from "./Input.props";
+import styles from './Input.module.scss';
+
+export const Input = ({}: InputProps) => {
+    return (
+        <input type="text" />
+    )
+}
