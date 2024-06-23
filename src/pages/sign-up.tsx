4cfe648552a9c57038850255cba9078d9a320458
Zrossiz <soon.pluxury@mail.ru>
2024-06-23 10:19:14 +0300

    chore: added metas for sign-up

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,17 +1,24 @@
 import { RegistrationForm } from '@/pageComponents';
 import styles from '../styles/SignUp.module.scss';
 import Image from 'next/image';
+import Head from 'next/head';
 
 const SignUpPage = () => {
     return (
-        <div className={styles.pageWrapper}>
-            <div className={styles.background}>
-                <Image src='/Background.jpg' fill alt="Background" />
+        <>
+            <Head>
+                <title>Cloud storage</title>
+                <link rel="icon" type='image/x-icon' href="/Logo.png" />
+            </Head> 
+            <div className={styles.pageWrapper}>
+                <div className={styles.background}>
+                    <Image src='/Background.jpg' fill alt="Background" />
+                </div>
+                <div className={styles.formWrapper}>
+                    <RegistrationForm />
+                </div>
             </div>
-            <div className={styles.formWrapper}>
-                <RegistrationForm />
-            </div>
-        </div>
+        </>
     )
 };
 
