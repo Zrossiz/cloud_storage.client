afece535b855540a313f5ee26d889b55632a70f1
Zrossiz <soon.pluxury@mail.ru>
2024-06-25 09:57:33 +0300

    feat: added upload file api function

--- src/api/File.ts
+++ src/api/File.ts
@@ -24,4 +24,30 @@ export const getFilesByPath = async (path: string = "/", cookies?: string): Prom
       }
       return { message: "Internal server error" };
     }
-  };
+};
+
+export const uploadFile = async (path: string = "/", file: File, cookies?: string): Promise<IFileObj[] | { message: string }> => {
+  try {
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('path', path);
+
+    const headers: Record<string, string> = {};
+    if (cookies) {
+      headers.Cookie = cookies;
+    }
+
+    const response = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}file/upload-file/`, formData, {
+      withCredentials: true,
+      headers
+    });
+
+    return response.data.data;
+  } catch (err: any) {
+    console.error("Error: ", err);
+    if (err.response && err.response.status === 401) {
+      return { message: "Unauthorized" };
+    }
+    return { message: "Internal server error" };
+  }
+}
